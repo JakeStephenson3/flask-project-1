68b2efb7f998891caf442c8ab3055e02a9068944
JakeStephenson3 <j.stephenson11@newcastle.ac.uk>
2023-09-26 17:45:13 +0100

    it's finished

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 from blog.views import blog_blueprint
-from main.views import main_blueprint
-from users.views import users_blueprint
 app.register_blueprint(blog_blueprint)
+from main.views import main_blueprint
 app.register_blueprint(main_blueprint)
+from users.views import users_blueprint
 app.register_blueprint(users_blueprint)
 
 @app.route('/')
--- users/views.py
+++ users/views.py
@@ -3,14 +3,14 @@ from flask import Blueprint, render_template
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
 
 
-@users_blueprint.route('/')
+@users_blueprint.route('/register')
 def register():
     return render_template('users/register.html')
 
-@users_blueprint.route('/')
+@users_blueprint.route('/login')
 def login():
     return render_template('users/login.html')
 
-@users_blueprint.route('/')
+@users_blueprint.route('/account')
 def account():
     return render_template('users/account.html')
