f1d6e3499a522a984374937fe3d2c784b8fec104
JakeStephenson3 <j.stephenson11@newcastle.ac.uk>
2023-09-26 17:05:07 +0100

    templates working

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'hi'
+    return render_template("helloworld.html")
 
 
 if __name__ == '__main__':
