ab6528ed857faf0f790faa2fb95421f96569e07e
JakeStephenson3 <j.stephenson11@newcastle.ac.uk>
2023-10-05 13:28:36 +0100

    Added data validation for username and passwords

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, ValidationError, length
+import re
 
+
+def character_check(self, field):
+    excluded_chars = "<&%"
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(f"Character {char} is not allowed.")
+
+
+def password_validate(self, field):
+    p = re.compile(r'(?=.*\d)(?=.*[a-z])')
+    if not p.match(field.data):
+        raise ValidationError("Password must contain at least 1 digit and one lowercase character.")
 
 class RegisterForm(FlaskForm):
-    username = StringField()
-    password = PasswordField()
+    username = StringField(validators=[DataRequired(), Email(), character_check])
+    password = PasswordField(validators=[DataRequired(), character_check, length(min=8, max=15), password_validate])
     submit = SubmitField()
+
