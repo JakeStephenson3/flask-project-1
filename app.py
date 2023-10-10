02195e79d53de8fc747af6d8b5387f0d396f58d2
JakeStephenson3 <j.stephenson11@newcastle.ac.uk>
2023-10-10 16:48:47 +0100

    Error handling added

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import secrets
 from flask import Flask, render_template
-
 app = Flask(__name__)
-
 from blog.views import blog_blueprint
 app.register_blueprint(blog_blueprint)
 from main.views import main_blueprint
@@ -11,6 +9,22 @@ from users.views import users_blueprint
 app.register_blueprint(users_blueprint)
 
 app.config['SECRET_KEY'] = secrets.token_hex(16)
+
+
+@app.errorhandler(403)
+def access_denied(error):
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
+
 
 @app.route('/')
 def hello_world():  # put application's code here
