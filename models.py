62272c62f7d895cd9bcce1e532f0d23b0a200fc1
JakeStephenson3 <j.stephenson11@newcastle.ac.uk>
2023-11-14 15:12:41 +0000

    Fixed typo crashing program, changed DateTime to DateTime

--- models.py
+++ models.py
@@ -19,7 +19,7 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), db.ForeignKey(User.username), nullable=True)
-    created = db.Column(db.Datetime, nullable=False)
+    created = db.Column(db.DateTime, nullable=False)
     title = db.Column(db.String, nullable=False)
     body = db.Column(db.String, nullable=False)
 
